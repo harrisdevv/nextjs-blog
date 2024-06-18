e3d9373532b19ebb6505bf77d3b352f82fb44ab7
Harrison Phan <harrisonphan5@gmail.com>
2024-06-18 20:19:19 +0700

    make tech skills nav animate swing around

--- src/component/AboutSection.tsx
+++ src/component/AboutSection.tsx
@@ -5,8 +5,8 @@ import SlideLeftToRightComponent from "./SlideLeftToRightComponent";
 
 const AboutSection = () => {
   return (
+    <SlideLeftToRightComponent>
       <section className="w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
-    <SlideLeftToRightComponent x={-100}>
         <div className="w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center">
           <Image
             src={"/coworking-male-programmer-writing-program-code.gif"}
@@ -18,9 +18,7 @@ const AboutSection = () => {
             sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
           />
         </div>
-        </SlideLeftToRightComponent>
 
-    <SlideLeftToRightComponent x={1000}>
         <div className="w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16">
           <h2 className="font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left">
             Innovate, Code, Transform!
@@ -34,8 +32,8 @@ const AboutSection = () => {
             Purpose, Making A Tangible Difference In The Tech World.
           </p>
         </div>
-    </SlideLeftToRightComponent>
       </section>
+    </SlideLeftToRightComponent>
   );
 };
 
