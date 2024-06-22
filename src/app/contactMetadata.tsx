3d605b9e18d0f969f1661ba1eaeb5678be22d4a4
Harrison Phan <harrisonphan5@gmail.com>
2024-06-22 22:36:40 +0700

    add video siteMetadata

--- src/app/contactMetadata.tsx
+++ src/app/contactMetadata.tsx
@@ -1,7 +1,27 @@
 import siteMetadata from "@/util/SourceMetaData";
+import { areIntervalsOverlapping } from "date-fns";
 
 
 export const metadata = {
   title: "Contact Me",
   description: `Contact me through the form available on this page or email me at ${siteMetadata.email}`,
 };
+
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class contactMetadata extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>
+
+        <video
+      </div>
+    )
+  }
+}
