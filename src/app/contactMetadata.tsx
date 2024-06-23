3022d76c14adf233dd0ebfdebb869dad2296dc60
Harrison Phan <harrisonphan5@gmail.com>
2024-06-23 22:47:37 +0700

    add alternative description

--- src/app/contactMetadata.tsx
+++ src/app/contactMetadata.tsx
@@ -4,4 +4,6 @@ import siteMetadata from "@/util/SourceMetaData";
 export const metadata = {
   title: "Contact Me",
   description: `Contact me through the form available on this page or email me at ${siteMetadata.email}`,
+  description2: `Contact me through the form available on this page or email me at ${siteMetadata.email}`,
+  description3: `Contact me through the form available on this page or email me at ${siteMetadata.email}`,
 };
